6622307147c72c1e9ccb15a507f702c2f1e160e2
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2022-10-28 22:37:58 +0330

    final test

--- js files/prices for services script file.js
+++ js files/prices for services script file.js
@@ -5,7 +5,7 @@
 // for(var i = 0; i < Jasan.length; i++){
 //     DubAndJasan.push(Jasan[i])
 // }
-    fetch("./data/Jasan wood data.json")
+    fetch('Jasan wood data.json')
     .then(response => response.json())
     .then(data => console.log(data))
 // window.addEventListener("load" , () => {
